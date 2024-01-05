65ab99665af6cd9c3c18fb03e650f009dd8803f9
Ramonee <107966914+PedroRamonee@users.noreply.github.com>
2024-01-05 17:01:54 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function alerta(elemento){
+   
+
+    elemento.style.position = 'absolute';
+    elemento.style.top = `${Math.floor(Math.random() * 40)}rem `;
+    elemento.style.left = `${Math.floor(Math.random() * 70)}rem `;
+}
